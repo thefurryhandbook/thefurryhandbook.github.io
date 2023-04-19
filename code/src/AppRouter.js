955591c6e2c8f5565252fe1a1f1b720d17f77ebc
Trevor Welch <103154412+Delaxiox@users.noreply.github.com>
2023-04-19 17:18:17 -0400

    Initial Code

---
+++ code/src/AppRouter.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import {BrowserRouter as Router, Route, Link, Routes} from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage';
+import NotFoundPage from './pages/NotFoundPage';
+
+function AppRouter() {
+return (
+    <Router>
+        <div>
+            <nav>
+            <ul>
+                <li>
+                <Link to="/">Home</Link>
+                </li>
+                <li>
+                <Link to="/about">About</Link>
+                </li>
+                <li>
+                <Link to="/contact">Contact</Link>
+                </li>
+            </ul>
+            </nav>
+
+            <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/contact" element={<ContactPage />} />
+            <Route path="*" element={<NotFoundPage />} />
+            </Routes>
+        </div>
+    </Router>
+    ); 
+}
+    
+  
+export default AppRouter;
